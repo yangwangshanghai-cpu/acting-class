07de2cf57b4e490ecb2b81c2d62c9b5096cfc8f8
yangwangshanghai-cpu <yangwangshanghai@gmail.com>
2026-05-02 01:56:00 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         我的GPT5.5 表演工具        </p>
+       <input placeholder="输入表演主题"/>
+<button>生成创意脚本</button>
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
